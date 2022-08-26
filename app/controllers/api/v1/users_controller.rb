6a8f13ec96c478954245954bd73941dbaeda2f5b
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-08-26 08:39:10 +0500

    Add user-destroyupdate tests, routes, actions

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :set_user, only: %i[ show update ]
+  before_action :set_user, only: %i[ show update destroy]
   # Get /users/1
   def show
     render json: User.find(params[:id])
@@ -16,6 +16,7 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
       render json: @user, status: :ok
@@ -24,6 +25,12 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  # DELETE /users/1
+  def destroy
+    @user.destroy
+    head 204 #means No_Content
+  end
+  
   private
 
   # Only allow a trusted parameter "white list" through.
