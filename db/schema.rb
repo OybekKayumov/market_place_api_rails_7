917fba839aaeb69b1bac327438bc9e65ab62b3ee
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-09-03 08:34:24 +0500

    Add modul Product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_26_020016) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_03_033246) do
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.decimal "price"
+    t.boolean "published"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "password_digest", null: false
@@ -19,4 +29,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_26_020016) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "products", "users"
 end
