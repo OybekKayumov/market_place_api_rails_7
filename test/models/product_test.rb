fd4b871d179a33dfeccc70f9ff9573c14738dbc6
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-09-03 11:32:22 +0500

    Add test and validations to products

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,9 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should have a positive price" do
+    product = products(:one)
+    product.price = -1
+    assert_not product.valid?
+  end
 end
