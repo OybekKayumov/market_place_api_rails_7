10faec97c7feeca1ab675cfc61920dd830dbb548
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-09-04 14:52:19 +0500

    Allows the dds order confirmation mailer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_04_070031) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_04_093812) do
   create_table "orders", force: :cascade do |t|
     t.integer "user_id", null: false
     t.decimal "total"
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_04_070031) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
--- test/controllers/api/v1/orders_controller_test.rb
+++ test/controllers/api/v1/orders_controller_test.rb
@@ -7,8 +7,10 @@ class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
   setup do
     # @order = orders(:one) 
     @order_params = { order: {
-      product_ids: [products(:one).id, products(:two).id], 
-      total: 50
+      product_ids_and_quantities: [
+        { product_id: products(:one).id, quantity: 2 }, 
+        { product_id: products(:two).id, quantity: 3 },
+      ]
     } }
   end
   
@@ -47,5 +49,15 @@ class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
     include_product_attr = json_response['included'][0]['attributes']
     assert_equal @order.products.first.title, 
     include_product_attr['title']
+  end
+
+  test 'should create order with two products and placements' do
+    assert_difference('Order.count', 1) do 
+    assert_difference('Placement.count', 2) do
+      post api_v1_orders_url, params: @order_params, as: :json
+        headers: { Authorization: JsonWebToken.encode(user_id: @order.user_id) }, 
+      end
+    end
+    assert_response :created 
   end
 end
